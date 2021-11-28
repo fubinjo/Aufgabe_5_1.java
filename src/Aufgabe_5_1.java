03c3c2e38153f8978b81928e49f18fb758d6620e
fubinjo <s2110238030@fhooe.at>
2021-11-28 20:06:22 +0100

    inital-commit

---
+++ src/Aufgabe_5_1.java
@@ -0,0 +1,26 @@
+import lib.util.In;
+import lib.util.Out;
+
+/**
+ *  Aufgabe 5 1
+ *
+ * Erstellen Sie eine Java Methode
+ * long digitSum(long n)
+ * Ist der Wert des Parameters n positiv, liefert die Methode die Quersumme der Zahl
+ * zurück, ansonsten -1.
+ *  Verwenden Sie diese Methode in einem Java Programm, das (wiederholbar) eine
+ * Zahl einliest und das Ergebnis der Methode ausgibt.
+ * ...
+ *
+ * @author Fabian Kostner, S2110238030 (Gruppe 4)
+ */
+
+public class Aufgabe_5_1 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
